8c8912d11adc66d4d2ab135d9bf516b71bff8325
Vaansh Lakhwara <vaanshlakhwara@gmail.com>
2021-04-06 21:02:27 +0400

    apollo-server-express-setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,29 @@
 import { MikroORM } from "@mikro-orm/core";
-import { Post } from "./entities/Post";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
 import microConfig from "./mikro-orm.config";
+import express from "express";
+import { HelloResolver } from "./resolvers/hello";
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
   await orm.getMigrator().up();
-  const post = orm.em.create(Post, { title: "first post" });
-  await orm.em.persistAndFlush(post);
 
-  const posts = await orm.em.find(Post, {});
-  console.log(posts);
+  const app = express();
+  const port = 4000;
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false,
+    }),
+  });
+
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(port, () => {
+    console.log(`listening at http://localhost:${port}`);
+  });
 };
 
 main().catch((err) => {
