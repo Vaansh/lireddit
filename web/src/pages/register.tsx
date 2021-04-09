3261af98b13a6e41ad835c02a1cfdb21855bfdd2
Vaansh Lakhwara <vaanshlakhwara@gmail.com>
2021-04-09 17:26:04 +0400

    urql-basic

--- web/src/pages/register.tsx
+++ web/src/pages/register.tsx
@@ -11,16 +11,39 @@ import {
 import { Form, Formik } from "formik";
 import { Wrapper } from "../components/Wrapper";
 import { InputField } from "../InputField";
+import { useMutation } from "urql";
 
 interface registerProps {}
 
+const REGISTER_MUT = `
+  mutation Register($username: String!, $password: String!){
+    register(options: {username: $username, password:$password}){
+      errors{
+        field
+        message      
+      }
+      user{
+        id
+        createdAt
+        updatedAt
+        username
+      }
+    }
+  }
+`;
+
 export const Register: React.FC<registerProps> = ({}) => {
+  // information about the data -1st parameter
+  // register is the function -2nd parameter
+  const [, register] = useMutation(REGISTER_MUT);
   return (
     <Wrapper variant="small">
       <Formik
         initialValues={{ username: "", password: "" }}
-        onSubmit={(values) => {
-          console.log(values);
+        onSubmit={async (values) => {
+          // if variables and mutation strings were different:
+          const response = await register(values);
+          // return  register({ username: values.user });
         }}
       >
         {({ isSubmitting }) => (
