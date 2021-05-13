a4049f79641cc2a505354ea29d55acc4051492c4
Vaansh Lakhwara <vaanshlakhwara@gmail.com>
2021-05-13 15:34:56 +0400

    change-vote

--- server/src/resolvers/post.ts
+++ server/src/resolvers/post.ts
@@ -53,25 +53,56 @@ export class PostResolver {
     const isUpdoot = value !== -1;
     const realValue = isUpdoot ? 1 : -1;
     const { userId } = req.session;
+    const updoot = await Updoot.findOne({ where: { postId, userId } });
+
+    // voted on post before
+    // changing vote
+    if (updoot && updoot.value !== realValue) {
+      await getConnection().transaction(async (tm) => {
+        await tm.query(
+          `
+          update updoot
+          set value = $1
+          where "postId" = $2 and "userId" = $3;
+          `,
+          [realValue, postId, userId]
+        );
+
+        await tm.query(
+          `
+          update post
+          set points = points + $1
+          where id = $2;
+          `,
+          [2 * realValue, postId]
+        );
+      });
+    } else if (!updoot) {
+      // never voted before
+      await getConnection().transaction(async (tm) => {
+        await tm.query(
+          `
+          insert into updoot("userId", "postId", value)
+          values ($1, $2, $3);
+          `,
+          [userId, postId, realValue]
+        );
+        await tm.query(
+          `
+          update post
+          set points = points + $1
+          where id = $2;
+          `,
+          [realValue, postId]
+        );
+      });
+    }
     // Updoot.insert({
     //   userId,
     //   postId,
     //   value: realValue,
     // });
-    await getConnection().query(
-      `
-    START TRANSACTION;
 
-    insert into updoot("userId", "postId", value)
-    values (${userId},${postId},${realValue});
-    
-    update post
-    set points = points + ${realValue}
-    where id = ${postId};
-    
-    COMMIT;
-    `
-    );
     return true;
   }
 
@@ -109,7 +140,7 @@ export class PostResolver {
       `,
       replacements
     );
-    console.log(posts);
+
     const qb = getConnection()
       .getRepository(Post)
       .createQueryBuilder("p")
