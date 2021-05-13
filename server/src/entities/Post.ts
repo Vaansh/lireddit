084a78c67b105d0975e9d5adb3a757bfeb3efe2c
Vaansh Lakhwara <vaanshlakhwara@gmail.com>
2021-05-13 16:10:45 +0400

    vote-status

--- server/src/entities/Post.ts
+++ server/src/entities/Post.ts
@@ -1,4 +1,4 @@
-import { Field, ObjectType } from "type-graphql";
+import { Field, Int, ObjectType } from "type-graphql";
 import {
   Entity,
   PrimaryGeneratedColumn,
@@ -30,6 +30,9 @@ export class Post extends BaseEntity {
   @Field()
   @Column({ type: "int", default: 0 })
   points!: number;
+
+  @Field(() => Int, { nullable: true })
+  voteStatus: number | null;
 
   @Field()
   @Column()
