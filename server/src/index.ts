b62d16520be585c001f570d9b1e2468b43987747
Vaansh Lakhwara <vaanshlakhwara@gmail.com>
2021-05-14 01:05:48 +0400

    migration

--- server/src/index.ts
+++ server/src/index.ts
@@ -24,7 +24,7 @@ const main = async () => {
     type: "postgres",
     url: process.env.DATABASE_URL,
     logging: true,
-    synchronize: true,
+    // synchronize: true,
     migrations: [path.join(__dirname, "./migrations/*")],
     entities: [Post, User, Updoot],
   });
